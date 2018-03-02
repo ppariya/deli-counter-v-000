6b64b72b280f8307ab183222b84d7f945e8dcb8e
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-02 14:27:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   else
     string = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      string.pop(" #{index+1}. #{name}")
+      string << (" #{index+1}. #{name}")
     end
     puts string
 
