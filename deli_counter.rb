d9c2701feb6fa8b928009d967d56a64265b9e1b5
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-02 13:56:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,6 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    "The line is currently: "+ katz_deli[0..-1].join(katz_deli[]+1)
+    "The line is currently: "+ katz_deli[0..-1].join(1..9)
   end
 end
