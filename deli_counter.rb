43427c96082305bcce1f1e24ab17f80a2b6d6587
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-02 14:30:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,6 +8,10 @@ def line(katz_deli)
       string << (" #{index+1}. #{name}")
     end
     puts string
-
   end
+end
+
+def take_a_number(katz_deli, name)
+  number = katz_deli.length+1
+  puts "Welcome, #{name}. You are number #{number} in line."
 end
